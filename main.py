1fa5e4912bf0557b3acb4f9fcb5cc13f750421cf
Matthew Clarke <96544215+matthewfloydclarke@users.noreply.github.com>
2021-12-23 15:23:59 +0000

    Imported all python files and created if statement

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import ai, nlt, database, conversation      #Imports all other python files
 
+#If statement allows only the user to run the code
+if __name__ == "__main__":
+    print("Hello World!")
