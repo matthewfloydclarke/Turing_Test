fce02ce1b0b6472b238a6d4527e9d9f7bdca3957
Matthew Clarke <96544215+matthewfloydclarke@users.noreply.github.com>
2021-12-29 20:03:14 +0000

    Created instance of User

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import ai, nlt, database, conversation      #Imports all other python files
 #If statement allows only the user to run the code
 if __name__ == "__main__":
     print("Hello World!")
+    database.createDatabase()
+
+    user = database.User(None, None, None)
+    user.saveid()
+    print("user's id is",user.id)
