af2dab0e095cf9c07a687ac752103fb2648a3730
Matthew Clarke <96544215+matthewfloydclarke@users.noreply.github.com>
2021-12-30 00:51:05 +0000

    Adds name and age individually to the table

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import ai, nlt, database, conversation      #Imports all other python files
 #If statement allows only the user to run the code
 if __name__ == "__main__":
     print("Hello World!")
-    database.createDatabase()
+    database.createDatabase()                       #Creates the database in database.py
 
-    user = database.User(None, None, None)
-    user.saveid()
-    print("user's id is",user.id)
+    user = database.User(None, None, None)          #Initiates an instance of User from database.py
+    user.saveid()                                   #Saves the id into the Users table
+    print("User's id is",user.id)
 
-    name = input("What's your name? ")
-    user.setName(name)
+    user_reply = conversation.initialMessage()      #Gets the reply from the initial chatbot message
 
-    age = int(input("How old are you? "))
-    user.setAge(age)
+    name = input("What's your name? ")              #Asks for the user's name
+    user.setName(name)                              #Sets the input as user's name
+    user.saveName()                                 #Saves name into the table
 
-    user.saveUser()
+    age = int(input("How old are you? "))           #Asks for the user's age
+    user.setAge(age)                                #Sets the input as user's age
+    user.saveAge()                                  #Saves age into the table
+
