a7c46ee2110e315820826d247c3655392224908d
Matthew Clarke <96544215+matthewfloydclarke@users.noreply.github.com>
2021-12-30 20:49:17 +0000

    Created AI counter and most of structure variables

--- Code/ai.py
+++ Code/ai.py
@@ -2,6 +2,18 @@ import random as rand   #Imports random for random weight values and training sa
 import math     #Imports math for activation function
 
 class Structure:
-    def __init__(self, inputs, outputs):
-        self.inputs = inputs
-        self.outputs = outputs
+
+    numAIs = 0                              #A counter for the number of AIs are made
+
+    def __init__(self, inputs, numOfHiddens, numOfLayers,numOfOutputs):
+        self.inputs = inputs                #Sets inputs
+        self.numOfInputs = len(inputs)      #Sets number of inputs
+        self.numOfHiddens = numOfHiddens    #Sets number of hidden nodes
+        self.numOfLayers = numOfLayers      #Sets number of hidden layers
+        self.numOfOutputs = numOfOutputs    #Sets number of outputs
+        self.aiNum = Structure.numAIs       #Sets what the AI number is
+        Structure.numAIs += 1               #Increases the counter
+
+    #Initialises the weights for the layers
+    def Initialise(self):
+        weights = []                        #List of weights
