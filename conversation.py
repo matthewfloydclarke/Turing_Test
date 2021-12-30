212030996f1027be34f2c3665881df0c6f3ef914
Matthew Clarke <96544215+matthewfloydclarke@users.noreply.github.com>
2021-12-30 00:50:21 +0000

    Changed end of print statement

--- conversation.py
+++ conversation.py
@@ -1,1 +1,15 @@
+import nlt
 
+#Prints the first message of the chatbot
+def initialMessage():
+    print("-> ",end="")
+    print("So you must be...")
+    answer = input("-> ")
+    return answer
+
+#Prints the replies of the chatbot
+def chatbotReply():
+    print("-> ",end="")
+    print("Hola")
+    answer = input("-> ")
+    return answer
