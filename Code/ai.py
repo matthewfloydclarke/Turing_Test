deba1ba25801a4272c7eba2f2163376e3e8b2cde
Matthew Clarke <96544215+matthewfloydclarke@users.noreply.github.com>
2021-12-30 19:25:42 +0000

    Moved code into Code file and added documentation document

---
+++ Code/ai.py
@@ -0,0 +1,7 @@
+import random as rand   #Imports random for random weight values and training sample selection
+import math     #Imports math for activation function
+
+class Structure:
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
